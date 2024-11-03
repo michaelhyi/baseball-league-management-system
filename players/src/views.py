379549ae747f2c52581ebf1cf6ff84fb425a9359
michaelhyi <michaelyi2004@gmail.com>
2024-11-03 15:27:45 -0500

    refactor: abstracted controller error handling logic

--- players/src/views.py
+++ players/src/views.py
@@ -7,26 +7,36 @@ from django.views.decorators.http import require_http_methods
 from players.src.models import PlayerNotFoundError, PlayerService, Position
 
 
+def handle_errors(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError as e:
+            return JsonResponse({"error": str(e)}, status=400)
+        except KeyError as e:
+            return JsonResponse({"error": str(e)}, status=400)
+        except PlayerNotFoundError as e:
+            return JsonResponse({"error": str(e)}, status=404)
+        except DatabaseError as e:
+            return JsonResponse({"error": "Internal server error"}, status=500)
+
+    return wrapper
+
+
 @require_http_methods(["POST"])
+@handle_errors
 def create_player(request):
-    try:
-        body = json.loads(request.body)
+    body = json.loads(request.body)
 
-        name = body.get("name")
-        age = body.get("age")
-        height = body.get("height")
-        weight = body.get("weight")
-        position = Position[body.get("position")]
-        team_id = body.get("teamId")
+    name = body.get("name")
+    age = body.get("age")
+    height = body.get("height")
+    weight = body.get("weight")
+    position = Position[body.get("position")]
+    team_id = body.get("teamId")
 
-        PlayerService.create_player(name, age, height, weight, position, team_id)
-        return JsonResponse({"message": "Player created"}, status=201)
-    except ValueError as e:
-        return JsonResponse({"error": str(e)}, status=400)
-    except KeyError as e:
-        return JsonResponse({"error": str(e)}, status=400)
-    except DatabaseError as e:
-        return JsonResponse({"error": "Internal server error"}, status=500)
+    PlayerService.create_player(name, age, height, weight, position, team_id)
+    return JsonResponse({"message": "Player created"}, status=201)
 
 
 @require_http_methods(["GET", "PATCH", "DELETE"])
@@ -39,50 +49,30 @@ def player_view(request, id):
         return delete_player(request, id)
 
 
+@handle_errors
 def get_player(request, id):
-    try:
-        player = PlayerService.get_player(id)
-        return JsonResponse(player.__dict__, status=200)
-    except ValueError as e:
-        return JsonResponse({"error": str(e)}, status=400)
-    except PlayerNotFoundError as e:
-        return JsonResponse({"error": str(e)}, status=404)
-    except DatabaseError as e:
-        return JsonResponse({"error": "Internal server error"}, status=500)
+    player = PlayerService.get_player(id)
+    return JsonResponse(player.__dict__, status=200)
 
 
 @require_http_methods(["PATCH"])
+@handle_errors
 def update_player(request, id):
-    try:
-        body = json.loads(request.body)
+    body = json.loads(request.body)
 
-        name = body.get("name")
-        age = body.get("age")
-        height = body.get("height")
-        weight = body.get("weight")
-        position = Position[body.get("position")]
-        team_id = body.get("teamId")
+    name = body.get("name")
+    age = body.get("age")
+    height = body.get("height")
+    weight = body.get("weight")
+    position = Position[body.get("position")]
+    team_id = body.get("teamId")
 
-        PlayerService.update_player(id, name, age, height, weight, position, team_id)
-        return JsonResponse({"message": "Player updated"}, status=200)
-    except ValueError as e:
-        return JsonResponse({"error": str(e)}, status=400)
-    except KeyError as e:
-        return JsonResponse({"error": str(e)}, status=400)
-    except PlayerNotFoundError as e:
-        return JsonResponse({"error": str(e)}, status=404)
-    except DatabaseError as e:
-        return JsonResponse({"error": "Internal server error"}, status=500)
+    PlayerService.update_player(id, name, age, height, weight, position, team_id)
+    return JsonResponse({"message": "Player updated"}, status=200)
 
 
 @require_http_methods(["DELETE"])
+@handle_errors
 def delete_player(request, id):
-    try:
-        PlayerService.delete_player(id)
-        return JsonResponse({"message": "Player deleted"}, status=200)
-    except ValueError as e:
-        return JsonResponse({"error": str(e)}, status=400)
-    except PlayerNotFoundError as e:
-        return JsonResponse({"error": str(e)}, status=404)
-    except DatabaseError as e:
-        return JsonResponse({"error": "Internal server error"}, status=500)
+    PlayerService.delete_player(id)
+    return JsonResponse({"message": "Player deleted"}, status=200)
