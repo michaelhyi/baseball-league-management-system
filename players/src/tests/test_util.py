53f3f01a6b4244a4de3fbdbbac053d5ce11ff865
michaelhyi <michaelyi2004@gmail.com>
2024-11-04 11:32:24 -0500

    chore: finished unit tests for util

--- players/src/tests/test_util.py
+++ players/src/tests/test_util.py
@@ -2,7 +2,12 @@ import json
 from django.test import TestCase
 from django.db import DatabaseError
 from django.http import JsonResponse
-from src.util import PlayerNotFoundError, handle_view_errors, parse_json_body
+from src.util import (
+    PlayerNotFoundError,
+    PlayerServiceUtil,
+    handle_view_errors,
+    parse_json_body,
+)
 from src.models import Position
 from src.tests.helpers import TestRequest
 
@@ -15,7 +20,7 @@ def handle_errors_wrapper(exception=None):
 
 
 class UtilTest(TestCase):
-    def test_parse_body(self):
+    def test_parse_json_body(self):
         player = {
             "name": "Michael Yi",
             "age": 19,
@@ -42,9 +47,9 @@ class UtilTest(TestCase):
         self.assertEqual(res.status_code, 200)
 
     def test_handle_value_error(self):
-        res = handle_errors_wrapper(ValueError("ID must be positive"))
+        res = handle_errors_wrapper(ValueError("id must be positive"))
         self.assertEqual(
-            json.loads(res.content.decode())["error"], "ID must be positive"
+            json.loads(res.content.decode())["error"], "id must be positive"
         )
         self.assertEqual(res.status_code, 400)
 
@@ -64,3 +69,131 @@ class UtilTest(TestCase):
             json.loads(res.content.decode())["error"], "Internal server error"
         )
         self.assertEqual(res.status_code, 500)
+
+
+class PlayerServiceUtilTest(TestCase):
+    def test_validate_data_will_throw_when_id_not_positive(self):
+        try:
+            PlayerServiceUtil.validate_data(id=-1)
+        except ValueError as e:
+            self.assertRaises(ValueError)
+            self.assertEqual(str(e), "id must be positive")
+
+    def test_validate_data_will_throw_when_name_is_none(self):
+        try:
+            PlayerServiceUtil.validate_data(id=1, name=None)
+        except ValueError as e:
+            self.assertRaises(ValueError)
+            self.assertEqual(str(e), "name must not be empty")
+
+    def test_validate_data_will_throw_when_name_is_empty(self):
+        try:
+            PlayerServiceUtil.validate_data(id=1, name="")
+        except ValueError as e:
+            self.assertRaises(ValueError)
+            self.assertEqual(str(e), "name must not be empty")
+
+    def test_validate_data_will_throw_when_age_is_negative(self):
+        try:
+            PlayerServiceUtil.validate_data(id=1, name="Michael Yi", age=-1)
+        except ValueError as e:
+            self.assertRaises(ValueError)
+            self.assertEqual(str(e), "age must be positive")
+
+    def test_validate_data_will_throw_when_height_is_none(self):
+        try:
+            PlayerServiceUtil.validate_data(
+                id=1, name="Michael Yi", age=19, height=None
+            )
+        except ValueError as e:
+            self.assertRaises(ValueError)
+            self.assertEqual(str(e), "height must not be empty")
+
+    def test_validate_data_will_throw_when_height_is_empty(self):
+        try:
+            PlayerServiceUtil.validate_data(id=1, name="Michael Yi", age=19, height="")
+        except ValueError as e:
+            self.assertRaises(ValueError)
+            self.assertEqual(str(e), "height must not be empty")
+
+    def test_validate_data_will_throw_when_weight_is_negative(self):
+        try:
+            PlayerServiceUtil.validate_data(
+                id=1, name="Michael Yi", age=19, height="5' 10\"", weight=-1
+            )
+        except ValueError as e:
+            self.assertRaises(ValueError)
+            self.assertEqual(str(e), "weight must be positive")
+
+    def test_validate_data_will_throw_when_position_is_none(self):
+        try:
+            PlayerServiceUtil.validate_data(
+                id=1,
+                name="Michael Yi",
+                age=19,
+                height="5' 10\"",
+                weight=140,
+                position=None,
+            )
+        except ValueError as e:
+            self.assertRaises(ValueError)
+            self.assertEqual(str(e), "position must not be empty")
+
+    def test_validate_data_will_throw_when_position_is_empty(self):
+        try:
+            PlayerServiceUtil.validate_data(
+                id=1,
+                name="Michael Yi",
+                age=19,
+                height="5' 10\"",
+                weight=140,
+                position="",
+            )
+        except ValueError as e:
+            self.assertRaises(ValueError)
+            self.assertEqual(str(e), "position must not be empty")
+
+    def test_validate_data_will_throw_when_team_id_is_negative(self):
+        try:
+            PlayerServiceUtil.validate_data(
+                id=1,
+                name="Michael Yi",
+                age=19,
+                height="5' 10\"",
+                weight=140,
+                position="Shortstop",
+                team_id=-1,
+            )
+        except ValueError as e:
+            self.assertRaises(ValueError)
+            self.assertEqual(str(e), "team_id must be positive")
+
+    def test_validate_data_will_not_throw_when_data_is_valid(self):
+        try:
+            PlayerServiceUtil.validate_data(
+                id=1,
+                name="Michael Yi",
+                age=19,
+                height="5' 10\"",
+                weight=140,
+                position="Shortstop",
+                team_id=1,
+            )
+        except:
+            self.fail("validate_data should not throw an error")
+
+    def test_validate_height_will_throw_when_height_is_invalid(self):
+        try:
+            PlayerServiceUtil.validate_height('5 10"')
+            PlayerServiceUtil.validate_height("5' 10")
+        except ValueError as e:
+            self.assertRaises(ValueError)
+            self.assertEqual(
+                str(e), "Height must match the following pattern: <ft>'<in>\""
+            )
+
+    def test_validate_height_will_not_throw_when_height_is_valid(self):
+        try:
+            PlayerServiceUtil.validate_height("5' 10\"")
+        except:
+            self.fail("validate_height should not throw an error")
