94b6e749dc43b0ae5c0edb91992f943d8a732c2a
michaelhyi <michaelyi2004@gmail.com>
2024-11-04 10:58:59 -0500

    refactor: abstracted utils functions in player service

--- players/src/util.py
+++ players/src/util.py
@@ -1,8 +1,49 @@
+import json
 import re
+
+from django.db import DatabaseError
+from django.http import JsonResponse
+
+from src.models import Position
 
 
 class PlayerNotFoundError(Exception):
     pass
+
+
+def parse_json_body(request) -> tuple[str, int, str, int, Position, int]:
+    body = json.loads(request.body)
+
+    name = body["name"]
+    age = body["age"]
+    height = body["height"]
+    weight = body["weight"]
+
+    position = Position(body["position"])
+
+    # TODO: fix
+    # if body["position"] not in Position.__members__:
+    # raise ValueError("Invalid position")
+
+    team_id = body["teamId"]
+
+    return name, age, height, weight, position, team_id
+
+
+def handle_view_errors(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError as e:
+            return JsonResponse({"error": str(e)}, status=400)
+        except KeyError as e:
+            return JsonResponse({"error": str(e)}, status=400)
+        except PlayerNotFoundError as e:
+            return JsonResponse({"error": str(e)}, status=404)
+        except DatabaseError as e:
+            return JsonResponse({"error": "Internal server error"}, status=500)
+
+    return wrapper
 
 
 height_pattern = r"^(?:(\d{1,2})'(\d{1,2})\"|\d{1,2}'\s*\d{1,2}\s*\"|(?:(\d{1,2})\s*feet\s*(\d{1,2})\s*inches))$"
